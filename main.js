54911a1107e2f5e51573cd77dd696b232a5c65aa
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-03 11:22:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.getElementById("btn-10").addEventListener("click", btnFunc10);
 
 function btnFunc1() {
 // Input
-let userName = prompt("Please enter your name").toLowerCase();
+    let userName = prompt("Please enter your name").toLowerCase();
 
 // Check userName
     if (userName === "oliver") {
@@ -21,19 +21,42 @@ let userName = prompt("Please enter your name").toLowerCase();
     } else {
     	alert("We don’t have the same name.");
     }
-
 }
 
 function btnFunc2() {
-    
+    let height = prompt("How tall are you in centimeters?")
+
+    if (height >= 82) {
+        alert("All aboard!")
+    } else {
+        alert("Shorty.")
+    }
 }
 
 function btnFunc3() {
-    
+    let sign = prompt("Provide a number with a sign")
+
+    if (sign > 0) {
+        alert("Positive")
+    } else if (sign < 0) {
+        alert("Negative")
+    } else {
+        alert("Zero")
+    }
 }
 
 function btnFunc4() {
-    
+    let courseRecommendation = prompt("What is your grade average?")
+
+    if (courseRecommendation >= 90) {
+        alert("You will do well at the next level.")
+    } else if (courseRecommendation >= 70) {
+        alert("You should do well at the next level.")
+    } else if (courseRecommendation >= 50) {
+        alert("The next level may be difficult for you.")
+    } else {
+        alert("You may not go to the next level.")
+    }
 }
 
 function btnFunc5() {
