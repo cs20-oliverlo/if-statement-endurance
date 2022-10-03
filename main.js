1deb53700098df561e957d69dc34d1b2c3d52728
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-03 11:12:00 -0600

    awd

---
+++ main.js
@@ -0,0 +1,61 @@
+// Buttons
+
+document.getElementById("btn-1").addEventListener("click", btnFunc1);
+document.getElementById("btn-2").addEventListener("click", btnFunc2);
+document.getElementById("btn-3").addEventListener("click", btnFunc3);
+document.getElementById("btn-4").addEventListener("click", btnFunc4);
+document.getElementById("btn-5").addEventListener("click", btnFunc5);
+document.getElementById("btn-6").addEventListener("click", btnFunc6);
+document.getElementById("btn-7").addEventListener("click", btnFunc7);
+document.getElementById("btn-8").addEventListener("click", btnFunc8);
+document.getElementById("btn-9").addEventListener("click", btnFunc9);
+document.getElementById("btn-10").addEventListener("click", btnFunc10);
+
+function btnFunc1() {
+// Input
+let userName = prompt("Please enter your name").toLowerCase();
+
+// Check userName
+    if (userName === "oliver") {
+    	alert("Wow, we have the same name.");
+    } else {
+    	alert("We don’t have the same name.");
+    }
+
+}
+
+function btnFunc2() {
+    
+}
+
+function btnFunc3() {
+    
+}
+
+function btnFunc4() {
+    
+}
+
+function btnFunc5() {
+    
+}
+
+function btnFunc6() {
+    
+}
+
+function btnFunc7() {
+    
+}
+
+function btnFunc8() {
+    
+}
+
+function btnFunc9() {
+    
+}
+
+function btnFunc10() {
+    
+}
